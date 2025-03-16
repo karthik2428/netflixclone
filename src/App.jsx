7a602cce6990d7ebc15108e30409433e1839b028
Karthik Santhosh <148684937+karthik2428@users.noreply.github.com>
2025-03-16 14:08:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Home from './Pages/Home/Home'
 import{Routes,Route, useNavigate} from 'react-router-dom' 
-import Login from './pages/Login/Login'
+import Login from "./Pages/Login/Login";
 import Player from './Pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import {auth} from './firebase'
